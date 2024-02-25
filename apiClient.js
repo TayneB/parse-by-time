309ee5ae7889dcbd931c675add3cf8a769998f5a
Tayne Bilyard <taynebilyard@gmail.com>
2024-02-25 19:52:12 +1300

    client credentials generating correctly

--- apiClient.js
+++ apiClient.js
@@ -1,19 +1,14 @@
 const clientId = process.env.CLIENT_ID
 const clientSecret = process.env.CLIENT_SECRET
+const clientCredentials = btoa(`${clientId}:${clientSecret}`)
 const tokenUrl = 'https://www.warcraftlogs.com/oauth/token'
 const apiUrl = ``
 
+console.log(clientCredentials)
 const getAccessToken = async () => {
-  return await fetch(tokenUrl, {
+  const response = await fetch(tokenUrl, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-    },
-    body: `grant_type=client_credentials`,
   })
-    .then((res) => res.json())
-    .then((data) => data.access_token)
 }
 
-getAccessToken().then((token) => console.log(token))
+getAccessToken()
